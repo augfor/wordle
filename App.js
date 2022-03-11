3b253207712dd393916700bb81a327ac51422f75
Augusto Forero <a.forarc@gmail.com>
2022-03-10 21:22:27 -0500

    Implement game logic

--- App.js
+++ App.js
@@ -1,16 +1,62 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, SafeAreaView, ScrollView, Text, View } from 'react-native';
 
-import { colors } from './src/constants';
+import { CLEAR, colors, ENTER } from './src/constants';
 import Keyboard from './src/components/Keyboard';
 
 const NUMBER_OF_TRIES = 6;
+
+const copyArray = (arr) => {
+  return [...arr.map((rows) => [...rows])];
+};
 
 export default function App() {
   const word = 'hello';
   const letters = word.split('');
 
-  const rows = new Array(NUMBER_OF_TRIES).fill(new Array(letters.length).fill('a'));
+  const [rows, setRows] = useState(
+    new Array(NUMBER_OF_TRIES).fill(new Array(letters.length).fill(''))
+  );
+  const [curRow, setCurRow] = useState(0);
+  const [curCol, setCurCol] = useState(0);
+
+  const onKeyPressed = (key) => {
+    const updatedRows = copyArray(rows);
+
+    if (key === CLEAR) {
+      const prevCol = curCol - 1;
+
+      if (prevCol >= 0) {
+        updatedRows[curRow][prevCol] = '';
+
+        setRows(updatedRows);
+        setCurCol(prevCol);
+      }
+
+      return;
+    }
+
+    if (key === ENTER) {
+      if (curCol === rows[0].length) {
+        setCurRow(curRow + 1);
+        setCurCol(0);
+      }
+
+      return;
+    }
+
+    if (curCol < rows[0].length) {
+      updatedRows[curRow][curCol] = key;
+
+      setRows(updatedRows);
+      setCurCol(curCol + 1);
+    }
+  };
+
+  const isCellActive = (row, col) => {
+    return row === curRow && col === curCol;
+  };
 
   return (
     <SafeAreaView style={styles.container}>
@@ -18,16 +64,22 @@ export default function App() {
       <Text style={styles.title}>WORDLE</Text>
       <ScrollView style={styles.map}>
         {rows.map((row, i) => (
-          <View style={styles.row} key={i}>
-            {row.map((cell, i) => (
-              <View style={styles.cell} key={i}>
+          <View style={styles.row} key={`row-${i}`}>
+            {row.map((cell, j) => (
+              <View
+                style={[
+                  styles.cell,
+                  { borderColor: isCellActive(i, j) ? colors.lightgrey : colors.darkgrey },
+                ]}
+                key={`row-${i}-${j}`}
+              >
                 <Text style={styles.cellText}>{cell.toUpperCase()}</Text>
               </View>
             ))}
           </View>
         ))}
       </ScrollView>
-      <Keyboard />
+      <Keyboard onKeyPressed={onKeyPressed} />
     </SafeAreaView>
   );
 }
